d9d0a89a48d262a4cc2b05d2ed963237b35352d0
Rahul Kureel <r.dev2000@gmail.com>
2025-07-20 21:31:44 -0700

    2015: solve day 1 part-2

--- aoc-2015/src/solution/day_1.rs
+++ aoc-2015/src/solution/day_1.rs
@@ -4,7 +4,11 @@ use crate::solution::Solution;
 
 pub fn solve(input: &str) -> Result<Solution> {
     let part_1_solution: Option<String> = solve_part_1(input);
-    Ok(Solution { part_1: part_1_solution, part_2: Option::None })
+    let part_2_solution: Option<String> = solve_part_2(input);
+    Ok(Solution {
+        part_1: part_1_solution, 
+        part_2: part_2_solution
+    })
 }
 
 fn solve_part_1(input: &str) -> Option<String> {
@@ -20,4 +24,24 @@ fn solve_part_1(input: &str) -> Option<String> {
     }
      
     Some(String::from(floor.to_string()))
+}
+
+fn solve_part_2(input: &str) -> Option<String> {
+    let mut floor: i32 = 0;
+    let mut ind: u32 = 0;
+    for c in input.chars() {
+        ind += 1;
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => {
+                panic!("Unknown character in input file")
+            }
+        }
+        if floor < 0 {
+            break; 
+        }
+    }
+     
+    Some(String::from((ind).to_string()))
 }
